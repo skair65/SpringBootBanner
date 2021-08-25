cf81e8c11c4b326148a0bde10f21a66324763fac
Sachin Kesharwani <skair65@gmail.com>
2021-08-25 16:34:45 +0530

    addding variable

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -10,6 +10,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+		int i =10;
 	}
 
 }
