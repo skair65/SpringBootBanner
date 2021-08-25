46f8b47c1430a2d63a8137aa0554d1ebe777dd3f
Sachin Kesharwani <skair65@gmail.com>
2021-08-25 16:39:09 +0530

    changes for 103

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
