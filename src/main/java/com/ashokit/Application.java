9929c6bf802b68a449b292e78187b7cd6d84da60
skair65 <84712742+skair65@users.noreply.github.com>
2021-08-29 22:58:32 +0530

    Update Application.java

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -5,13 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-		int j =10;
-		int k =20;
 	}
 
 }
