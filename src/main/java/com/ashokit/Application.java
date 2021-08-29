204b5378617e4e55ece1953683c99c6b0d574d38
skair65 <84712742+skair65@users.noreply.github.com>
2021-08-29 23:04:07 +0530

    Update Application.java

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+// modifiedNothing
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
